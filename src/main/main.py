976c851503e74c8f69c5f8a7c050a99e5dce1368
emvanmetre <elizabethmvm@gmail.com>
2024-03-23 18:25:01 -0400

    access propelauth + reorg

--- src/main/main.py
+++ src/main/main.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, request
-import pandas as pd
+
+from propelauth_flask import init_auth, current_user
+
+auth = init_auth(
+    "https://59223397842.propelauthtest.com",
+    "608b45fdc719549f347b990b408a1159f800ad283d94dab0c11b60e4525114492ede59d57ff7ea8965f493f1f9965687",
+)
 
 app = Flask(__name__)
+
+
+@app.route("/api/whoami")
+@auth.require_user
+def who_am_i():
+    """This route is protected, current_user is always set"""
+    return {"user_id": current_user.user_id}
 
 
 @app.route("/")
