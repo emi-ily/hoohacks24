e376b06859a1f19d1d0016c4a1710f2608e4d3f4
Emily Chen <qeq3cf@virginia.edu>
2024-03-23 21:44:16 -0400

    merging

--- src/main/calculate.py
+++ src/main/calculate.py
@@ -1,3 +1,6 @@
+from flask import Flask
+
+
 # the values are greenhouse gas emissions per serving of food, as calcualted in ReadCSVcarbondata.py
 d = {'Cheese': 2.388, 'Coffee': 0.05706000000000001, 'Eggs': 0.467, 'Milk': 0.7875, 'Beef': 15.9168, 'Fish': 2.0445,
      'Lamb': 6.3552, 'Pork': 1.8465, 'Chicken': 1.5792}
@@ -5,3 +8,5 @@ d = {'Cheese': 2.388, 'Coffee': 0.05706000000000001, 'Eggs': 0.467, 'Milk': 0.78
 
 def calculate_emissions(food, servings):
     return d[food] * servings
+
+beefservings
